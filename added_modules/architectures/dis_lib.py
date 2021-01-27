c4c8e9d3f49d52692727390260bcaf14db33d420
luis-armando-perez-rey <57133495+luis-armando-perez-rey@users.noreply.github.com>
2021-01-27 09:49:14 +0100

    Update dis_lib.py

--- added_modules/architectures/dis_lib.py
+++ added_modules/architectures/dis_lib.py
@@ -3,25 +3,6 @@ import torch
 import numpy as np
 import torch.nn.functional as F
 
-
-def calculate_pad_same(image_size, kernel_size, stride):
-    """
-    Calculates the padding to get the "same" size as in Tensorflow
-    Only works for images were filter covers the complete image in the convolution
-    """
-    print((image_size[0] - (kernel_size[0] - 1) - 1) % stride[0] == 0)
-    print("Image size", image_size)
-    print("Kernel size", kernel_size)
-    print("Stride size", stride)
-    assert (image_size[0] - (kernel_size[0] - 1) - 1) % stride[
-        0] == 0, "Image can't be convoluted on the height exactly"
-    assert (image_size[1] - (kernel_size[1] - 1) - 1) % stride[1] == 0, "Image can't be convoluted on the width exactly"
-
-    pad = tuple(
-        [(image_size[num] * (stride[num] - 1) - stride[num] + kernel_size[num]) // 2 for num in range(len(image_size))])
-    return pad
-
-
 class Encoder(nn.Module):
 
     def __init__(self,
@@ -38,9 +19,7 @@ class Encoder(nn.Module):
         self.conv3 = nn.Conv2d(conv_hid, conv_hid*2, conv_kernel, stride=conv_stride, padding=1)
         self.conv4 = nn.Conv2d(conv_hid*2, conv_hid*2, conv_kernel, stride=conv_stride, padding=1)
 
-
-        final_size = np.product((conv_hid*2, 64//(2**4), 64//(2**4)))
-        self.fc1 = nn.Linear(final_size, 256)
+        self.fc1 = nn.Linear(64*4*4, 256)
         self.fc2 = nn.Linear(256, z_dim)
 
     def forward(self, x):
@@ -68,8 +47,6 @@ class Decoder(nn.Module):
         self.fc1 = nn.Linear(n_in, 256)
         self.fc2 = nn.Linear(256, 1024)
 
-        self.up1 = nn.Upsample(scale_factor=1)
-
         self.deconv1 = nn.ConvTranspose2d(64, 64, conv_kernel, stride=conv_stride, padding=1)
         self.deconv2 = nn.ConvTranspose2d(64, 32, conv_kernel, stride=conv_stride, padding=1)
         self.deconv3 = nn.ConvTranspose2d(32, 32, conv_kernel, stride=conv_stride, padding=1)
@@ -82,6 +59,6 @@ class Decoder(nn.Module):
         x = F.relu(self.deconv1(x))
         x = F.relu(self.deconv2(x))
         x = F.relu(self.deconv3(x))
-        x = F.relu(self.deconv4(x))
+        x = torch.sigmoid(self.deconv4(x))
 
-        return torch.sigmoid(x).squeeze(dim = 0)
+        return x.squeeze(dim = 0)
